8ec284d5ce9e4be98638427b2317214a2d0a1ab2
unknown <ralphlsulit@gmail.com>
2023-09-23 20:14:36 +0800

    feat: added Grid component in MuiLayout (Grid component)

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,38 +1,64 @@
-import { Box, Stack, Divider } from '@mui/material';
+import { Box, Stack, Divider, Grid } from '@mui/material';
 
 export const MuiLayout = () => {
   return( 
-    <Stack 
-      my={5} 
-      sx={{border: '1px solid'}} 
-      direction='row' 
-      spacing={2}
-      divider={<Divider orientation='vertical' flexItem />}
-    >
-      <Box 
-        sx={{
-          backgroundColor: 'primary.main',
-          color: 'white',
-          height: '100px',
-          width: '100px',
-          padding: '16px',
-          '&:hover': {
-            backgroundColor: 'primary.light'
-          }
-        }}
+    <>
+      <Stack 
+        my={5} 
+        sx={{border: '1px solid'}} 
+        direction='row' 
+        spacing={2}
+        divider={<Divider orientation='vertical' flexItem />}
       >
-        Box Component
-      </Box>
+        <Box 
+          sx={{
+            backgroundColor: 'primary.main',
+            color: 'white',
+            height: '100px',
+            width: '100px',
+            padding: '16px',
+            '&:hover': {
+              backgroundColor: 'primary.light'
+            }
+          }}
+        >
+          Box Component
+        </Box>
 
-      <Box
-        display='flex'
-        flexDirection='row'
-        height='100px'
-        width='100px'
-        bgcolor='success.light'
-        p={2}
-      >
-      </Box>
-    </Stack>
+        <Box
+          display='flex'
+          flexDirection='row'
+          height='100px'
+          width='100px'
+          bgcolor='success.light'
+          p={2}
+        >
+        </Box>
+      </Stack>
+
+      <Grid container rowSpacing={2} columnSpacing={1}>
+        <Grid item xs={6}>
+          <Box bgcolor='primary.light' p={2}>
+            Item 1
+          </Box>  
+        </Grid> 
+        <Grid item xs={6}>
+          <Box bgcolor='primary.light' p={2}>
+            Item 2
+          </Box>  
+        </Grid> 
+        <Grid item xs={6}>
+          <Box bgcolor='primary.light' p={2}>
+            Item 3
+          </Box>  
+        </Grid> 
+        <Grid item xs={6}>
+          <Box bgcolor='primary.light' p={2}>
+            Item 4
+          </Box>  
+        </Grid> 
+      </Grid>
+    
+    </>
   )
 }
